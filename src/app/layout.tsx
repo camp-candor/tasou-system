9175e050f5f1929e99db295dcf6a6453df843f66
Brad Henderson <elliotbradly@gmail.com>
2025-10-10 09:35:49 -0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,14 @@ import type { Metadata } from "next";
 import Providers from "../lib/query-provider";
 import "./globals.css";
 
-
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Next App Mantine Tailwind Template",
@@ -18,11 +25,24 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="antialiased">
-        <Providers>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-          </header>
-          {children}
-        </Providers>
+        <ClerkProvider>
+          <Providers>
+            <header className="flex justify-end items-center p-4 gap-4 h-16">
+              <SignedOut>
+                <SignInButton />
+                <SignUpButton>
+                  <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </header>
+            {children}
+          </Providers>
+        </ClerkProvider>
       </body>
     </html>
   );
